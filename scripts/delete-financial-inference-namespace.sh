619fac03b6116eecc02c68eef046aded0eeaa33e
jtayl222 <jtayl22,at,gmail,dot,com@obfuscated>
2025-07-11 14:01:37 -0400

    convenience script corrections

--- scripts/delete-financial-inference-namespace.sh
+++ scripts/delete-financial-inference-namespace.sh
@@ -5,10 +5,11 @@ kubectl patch experiment financial-ab-test-experiment -n financial-inference --t
 kubectl patch server mlserver -n financial-inference --type='merge' -p='{"metadata":{"finalizers":null}}' 2>/dev/null || true
 kubectl patch  seldonruntimes financial-inference-runtime -n financial-inference --type='merge' -p='{"metadata":{"finalizers":null}}' 2>/dev/null || true
 
+kubectl patch  namespace financial-inference --type='merge' -p='{"metadata":{"finalizers":null}}' 2>/dev/null || true
 
 kubectl delete model baseline-predictor -n financial-inference
 kubectl delete model enhanced-predictor -n financial-inference
 kubectl delete experiment financial-ab-test-experiment -n financial-inference
 kubectl delete server mlserver -n financial-inference
-kubectl delete namespace financial-inference
 kubectl delete seldonruntimes financial-inference-runtime -n financial-inference
+kubectl delete namespace financial-inference
